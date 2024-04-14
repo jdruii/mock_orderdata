393536cc918545426919c812f337ec501a4bf835
jdruii <146858759+jdruii@users.noreply.github.com>
2024-04-14 21:53:20 +0800

    Update producer.py
    - Serializes all messages
    - Optimizes serialization BY A TON

--- producer.py
+++ producer.py
@@ -1,17 +1,15 @@
-from kafka import KafkaProducer
 from confluent_kafka import SerializingProducer
 from confluent_kafka.serialization import StringSerializer
 from confluent_kafka.schema_registry import SchemaRegistryClient
 from confluent_kafka.schema_registry.json_schema import JSONSerializer
 from faker import Faker
-import json
 from datetime import datetime, timedelta
 import random
 import time
-from collections import Counter
 import iso_mapping
 import sku_info
 import hidden
+import schema
 
 fake = Faker('fil_PH')
 fake_bgy = Faker('es_CO')
@@ -27,79 +25,6 @@ producer = KafkaProducer(
     sasl_plain_password = secrets['sasl_plain_password'],
 )
 
-schema_str = """
-{
-  '$schema': 'http://json-schema.org/draft-07/schema#',
-  'title': 'Order',
-  'type': 'object',
-  'properties': {
-    'order_id': {
-      'type': 'integer'
-    },
-    'order_date': {
-      'type': 'string',
-      'format': 'date-time'
-    },
-    'customer_info': {
-      'type': 'object',
-      'properties': {
-        'customer_id': {'type': 'integer'},
-        'customer_name': {'type': 'string'},
-        'company': {'type': 'string'},
-        'email': {'type': 'string', 'format': 'email'},
-        'street_address': {'type': 'string'},
-        'lgu': {'type': 'string'},
-        'provincial_code': {'type': 'string'},
-        'contact_number': {'type': 'string'}
-      },
-      'required': ['customer_id', 'customer_name', 'email', 'company', 'street_address', 'lgu', 'provincial_code', 'contact_number']
-    },
-    'payment_info': {
-      'type': 'object',
-      'properties': {
-        'payment_status': {'type': 'boolean'},
-        'payment_method': {'type': 'string'},
-        'payment_date': {'type': 'string', 'format': 'date-time'}
-      },
-      'required': ['payment_status', 'payment_method', 'payment_date']
-    },
-    'shipping_info': {
-      'type': 'object',
-      'properties': {
-        'express_shipping': {'type': 'boolean'},
-        'shipped_date': {'type': 'string', 'format': 'date-time'},
-        'eta': {'type': 'string', 'format': 'date-time'},
-        'finished_date': {'type': 'string', 'format': 'date-time'},
-        'shipping_id': {'type': 'integer'},
-        'order_status': {'type': 'string'}
-      },
-      'required': ['shipping_id', 'order_status', 'express_shipping', 'shipped_date', 'eta', 'finished_date']
-    },
-    'item_info': {
-      'type': 'array',
-      'items': {
-        'type': 'object',
-        'properties': {
-          'sku': {'type': 'integer'},
-          'quantity': {'type': 'integer'},
-          'unit_price': {'type': 'number'},
-          'discount_percent': {'type': 'number'},
-          'subtotal': {'type': 'number'}
-        },
-        'required': ['sku', 'quantity', 'unit_price', 'discount_percent', 'subtotal']
-      }
-    },
-    'order_total': {
-      'type': 'number'
-    },
-    'item_variety': {
-      'type': 'integer'
-    }
-  },
-  'required': ['order_id', 'order_date', 'customer_info', 'payment_info', 'shipping_info', 'item_info', 'order_total', 'item_variety']
-}
-"""
-
 schema_registry_conf = {
     'url': secrets['url'],
     'basic.auth.user.info': secrets['basic.auth.user.inf']
@@ -107,7 +32,15 @@ schema_registry_conf = {
 
 schema_registry_client = SchemaRegistryClient(schema_registry_conf)
 
-json_serializer = JSONSerializer(schema_str, schema_registry_client)
+serializers = {
+    'Delivered': JSONSerializer(schema.finished_schema_str, schema_registry_client),
+    'Shipped': JSONSerializer(schema.shipped_schema_str, schema_registry_client),
+    'Paid': JSONSerializer(schema.paid_schema_str, schema_registry_client),
+    'Unpaid': JSONSerializer(schema.unpaid_schema_str, schema_registry_client),
+    'Timed Out': JSONSerializer(schema.failed_schema_str, schema_registry_client),
+    'Cancelled': JSONSerializer(schema.failed_schema_str, schema_registry_client),
+    'Returned': JSONSerializer(schema.failed_schema_str, schema_registry_client)
+}
 
 producer_conf = {
     'bootstrap.servers': secrets['bootstrap.servers'],
@@ -116,10 +49,13 @@ producer_conf = {
     'sasl.username': secrets['sasl.username'],
     'sasl.password': secrets['sasl.password'],
     'key.serializer': StringSerializer('utf_8'),
-    'value.serializer': json_serializer
 }
 
-serializing_producer = SerializingProducer(producer_conf)
+producers = {}
+for status, serializer in serializers.items():
+    specific_conf = producer_conf.copy()
+    specific_conf['value.serializer'] = serializer
+    producers[status] = SerializingProducer(specific_conf)
 
 order_counter = 0
 min_qty = 1
@@ -140,7 +76,6 @@ start_time = time.time()
 
 future_orders = False
 chosen_event = 'sale_event_0'
-order_frequencies = Counter()
 
 
 def generate_random_email_pattern(first_name: str, last_name: str, company_name: str) -> str:
@@ -569,16 +504,17 @@ def main():
         start_date = datetime.fromisoformat(order_data['order_date'])
 
         kafka_topic = determine_kafka_topic(order_data['shipping_info']['order_status'], order_data['payment_info']['payment_status'])
-        payment_status = order_data['payment_info']['payment_status']
-        if kafka_topic == 'finished_orders':
-            serializing_producer.produce(topic = kafka_topic, key = str(order_data['order_id']), value = order_data)
-            serializing_producer.flush()
-            print(f'Incoming Order: {order_counter:06} | Payment Status: {payment_status} | Sending to: {kafka_topic}')
+        order_status = order_data['shipping_info']['order_status']
+        specific_producer = producers.get(order_status)
+        if specific_producer:
+            specific_producer.produce(topic = kafka_topic, key = str(order_data['order_id']), value = order_data)
+            print(f'Incoming Order: {order_counter:06} | Order Status: {order_status} | Sent to: {kafka_topic}')
+            specific_producer.poll(0)
         else:
-            producer.send(kafka_topic, value = order_data)
-            print(f'Incoming Order: {order_counter:06} | Payment Status: {payment_status} | Sending to: {kafka_topic}')
-
-    producer.close()
+            print(f"Unsupported order status: {order_status}")
 
 if __name__ == '__main__':
     main()
+
+for producer in producers.values():
+    producer.flush()
